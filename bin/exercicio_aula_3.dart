ef49042d4e07773dd0b1139db7221e1191f039c0
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2026-05-15 22:23:44 -0300

    Refactor: Created class ListSales and changes main to use this

--- bin/exercicio_aula_3.dart
+++ bin/exercicio_aula_3.dart
@@ -2,20 +2,6 @@ import 'package:exercicio_aula_3/db/data.dart';
 import 'package:exercicio_aula_3/exercicio_aula_3.dart';
 
 void main(List<String> arguments) {
-  Iterable<Sales> vendas = listaPedidos.map(
-    (venda) => Sales(
-      idSale: venda['id_pedido'] as String,
-      createdAt: venda['data_criacao'] as String,
-      status: venda['status'] as String,
-      client: Client.fromApi(venda['cliente'] as Map),
-      payment: Payment.fomApi(venda['pagamento'] as Map),
-      itensList: ItemList.fromApi(venda['itens'] as List<Map<String, dynamic>>),
-    ),
-  );
-
-  for (var venda in vendas) {
-    print(
-      '[ID-VENDA : ${venda.idSale}] [ID-USUARIO : ${venda.client.idClient}][MÉTODO-PAGAMENTO : ${venda.payment.method}] \n[PRODUTOS : ${venda.itensList.itens.map((item) =>'ID: ${item.idItem} || NOME : ${item.name} || QUANTIDADE: ${item.quantity}] [VALOR UNITARIO ${item.unitValue}')}]\n',
-    );
-  }
+  ListSales listaVendas = ListSales.fromApi(listaPedidos);
+  listaVendas.printSales();
 }
--- lib/exercicio_aula_3.dart
+++ lib/exercicio_aula_3.dart
@@ -3,7 +3,7 @@ class Client {
   String name;
   String email;
   Client({required this.idClient, required this.name, required this.email});
-  Client.fromApi(Map map)
+  Client.fromMap(Map map)
     : idClient = map['id_cliente'],
       name = map['nome'],
       email = map['email'];
@@ -20,7 +20,7 @@ class Payment {
     required this.confirmed,
   });
 
-  Payment.fomApi(Map map)
+  Payment.fomMap(Map map)
     : method = map['metodo'],
       installment = map['parcelas'],
       confirmed = map['confirmado'];
@@ -43,18 +43,20 @@ class Item {
 class ItemList {
   List<Item> itens;
   ItemList({required this.itens});
-  ItemList.fromApi(List<Map<String,dynamic>> itemList)
-    : itens = itemList.map(
-        (item) => Item(
-          idItem: item['id_produto'] as String,
-          name: item['nome'] as String,
-          quantity: item['quantidade'] as int,
-          unitValue: (item['preco_unitario'] as num).toDouble(),
-        ),
-      ).toList();
+  ItemList.fromListMapStringDynamic(List<Map<String, dynamic>> itemList)
+    : itens = itemList
+          .map(
+            (item) => Item(
+              idItem: item['id_produto'] as String,
+              name: item['nome'] as String,
+              quantity: item['quantidade'] as int,
+              unitValue: (item['preco_unitario'] as num).toDouble(),
+            ),
+          )
+          .toList();
 }
 
-class Sales {
+class Sale {
   String idSale;
   String createdAt;
   String status; //todo : implement enum
@@ -62,7 +64,7 @@ class Sales {
   Payment payment;
   ItemList itensList;
 
-  Sales({
+  Sale({
     required this.idSale,
     required this.createdAt,
     required this.status,
@@ -70,4 +72,34 @@ class Sales {
     required this.payment,
     required this.itensList,
   });
+}
+
+class ListSales {
+  List<Sale> salesList;
+
+  ListSales({required this.salesList});
+
+  ListSales.fromApi(List<Map<String, Object>> list)
+    : salesList = list
+          .map(
+            (venda) => Sale(
+              idSale: venda['id_pedido'] as String,
+              createdAt: venda['data_criacao'] as String,
+              status: venda['status'] as String,
+              client: Client.fromMap(venda['cliente'] as Map),
+              payment: Payment.fomMap(venda['pagamento'] as Map),
+              itensList: ItemList.fromListMapStringDynamic(
+                venda['itens'] as List<Map<String, dynamic>>,
+              ),
+            ),
+          )
+          .toList();
+
+  void printSales() {
+    for (var venda in this.salesList) {
+      print(
+        '[ID-VENDA : ${venda.idSale}] [ID-USUARIO : ${venda.client.idClient}][MÉTODO-PAGAMENTO : ${venda.payment.method}] \n[PRODUTOS : ${venda.itensList.itens.map((item) => 'ID: ${item.idItem} || NOME : ${item.name} || QUANTIDADE: ${item.quantity}] [VALOR UNITARIO ${item.unitValue}')}]\n',
+      );
+    }
+  }
 }
